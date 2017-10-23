a99390a9d3ad804fc09177365c6d85401dbe651d
Thomas Valenta <thomasbvalenta@gmail.com>
2017-10-23 15:14:05 -0700

    Change filename to desired .lis, and write the first lines to that file

--- sicxe_asm.cpp
+++ sicxe_asm.cpp
@@ -6,3 +6,28 @@
 */
 
 #include "sicxe_asm.h"
+
+void sicxe_asm::write_to_file(string fileName) {
+    string firstLine[] = {"Line#","Address","Lable","Opcode","Operand"};
+    string secondLine[] = {"=====","=======","=====","======","======="};
+    fileName.erase((fileName.end()-3),fileName.end());
+    fileName.append("lis");
+
+    myfile.open(fileName);
+    int i = 0;
+    for (i = 0; i < 5; i++) {
+        myfile << firstLine[i] << "\t";
+    }
+    myfile << endl;
+    for (i = 0; i < 5; i++) {
+        myfile << secondLine[i] << "\t";
+    }
+    myfile << endl;
+}
+
+int main(int argc, char *argv[]){
+    string fileName = argv[1];
+
+    sicxe_asm::write_to_file(fileName);
+
+}
--- sicxe_asm.h
+++ sicxe_asm.h
@@ -5,3 +5,31 @@
  * Program 3
 */
 
+#ifndef SICXE_ASM
+#define SICXE_ASM
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "file_parser.h"
+#include "file_parse_exception.h"
+
+#include "opcodetab.h"
+#include "opcode_error_exception.h"
+
+#include "symtab.h"
+#include "symtab_exception.h"
+
+class sicxe_asm {
+
+public:
+
+
+private:
+    int lineNumber;
+
+    ofstream myfile;
+    void write_to_file(string fileName);
+
+};
