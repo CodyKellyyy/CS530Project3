e1364949eadfc7e6dfcff8a40d0dcc6c986933f9
edwin.coronado <edwin.coronado@servicenow.com>
2017-10-25 17:59:08 -0700

    Added functions and the outer loop for the parsing

--- sicxe_asm.cpp
+++ sicxe_asm.cpp
@@ -7,6 +7,74 @@
 
 #include "sicxe_asm.h"
 
+int main(int argc, char *argv[]){
+    //    if(argc != 2) {
+//        cout << "Error, you must supply the name of the file " <<
+//             "to process at the command line." << endl;
+//        exit(1);
+//    }
+    string filename = "/Users/edwin.coronado/CLionProjects/SIC-XE_Tokenizer/source4.txt";
+    try {
+        file_parser parser(filename); //Initialize file_parser object
+        symtab symbtab; //Initialize Symbol Table object
+
+        parser.read_file(); //Read the file (tokenize it)
+
+        sicxe_asm::parse_rows(parser);
+
+
+    }
+    catch(file_parse_exception excpt) {
+        cerr << "**Sorry, error: " << excpt.getMessage() << endl;
+    }
+
+}
+
+sicxe_asm::sicxe_asm() {}
+
+void sicxe_asm::parse_rows(file_parser parser) {
+
+    //This outer loop goes through each row in the file parser
+    for (int i = 0; i < parser.size(); i++) {
+        if (parser.get_token(i, OPCODE) != "END") {
+            //LOCCTR = format_address(parser.get_token(i,OPERANDS));
+        }
+    }
+}
+
+int sicxe_asm::format_address(string str_addr) {
+
+    bool is_hex;
+    bool is_dec;
+
+    size_t found_hex = str_addr.find("$");
+    if (found_hex != string::npos) {
+        is_hex = true;
+        is_dec = false;
+    } else {
+        is_dec = true;
+        is_hex = false;
+    }
+
+    if (is_hex) {
+        string temp = "";
+        for (int i = 0; i < str_addr.length(); i++) {
+            if (isdigit(str_addr[i]))
+                temp += str_addr[i];
+        }
+        return stoi(temp, nullptr, 16);
+    }
+    else if (is_dec) {
+        string temp = "";
+        for (int i = 0; i < str_addr.length(); i++) {
+            if (isdigit(str_addr[i]))
+                temp += str_addr[i];
+        }
+        return stoi(temp);
+    }
+}
+
+
 void sicxe_asm::write_to_file(string fileName) {
     string firstLine[] = {"Line#","Address","Lable","Opcode","Operand"};
     string secondLine[] = {"=====","=======","=====","======","======="};
@@ -26,26 +94,27 @@ void sicxe_asm::write_to_file(string fileName) {
 
 }
 
+bool sicxe_asm::is_assm_dir(string code) {
+    vector<string> assembler_directives;
+    assembler_directives.push_back("START");
+    assembler_directives.push_back("END");
+    assembler_directives.push_back("BYTE");
+    assembler_directives.push_back("WORD");
+    assembler_directives.push_back("RESB");
+    assembler_directives.push_back("RESW");
+    assembler_directives.push_back("BASE");
+    assembler_directives.push_back("NOBASE");
+
+    if (find(assembler_directives.begin(), assembler_directives.end(), code) != assembler_directives.end()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 //void sicxe_asm::add_to_symtab(string address, string label, string operand) {
 //    symtab symbol;
 //    symbol.add_symbol(string(label),int(0));
 //
 //}
 
-int main(int argc, char *argv[]){
-    //string fileName = argv[1];
-    symtab symtab1;
-    symtab1.add_symbol("EDWIN", 17);
-    symtab1.add_symbol("MCFLURRY", 24);
-
-    symtab1.print_map();
-
-    symtab1.delete_symbol("EDWIN");
-
-    symtab1.print_map();
-
-    cout << symtab1.get_value("MCFLURRY") << endl;
-    cout << symtab1.get_value("MCFaLURRY") << endl;
-
-
-}
--- sicxe_asm.h
+++ sicxe_asm.h
@@ -24,12 +24,22 @@
 class sicxe_asm {
 
 public:
+    sicxe_asm();
+    ~sicxe_asm();
+    static void parse_rows(file_parser);
+    static int format_address(string);
+    static bool is_assm_dir(string);
+
 
 
 private:
 
+    /* ---- Members ---- */
     // All values to be put into list file
     int lineNumber;
+    static int starting_address;
+    static int LOCCTR;
+
     string address;
     string label;
     string opcode;
@@ -37,10 +47,22 @@ private:
 
     ofstream myfile;
 
+    /* ---- Functions ---- */
     // Writes the Line number, address, label, opcode, and operand to the listing file
     void write_to_file(string fileName);
     // Adds the symbols in the code to symtab
     void add_to_symtab(string address, string label, string opcode);
+    // Formats the address depending if it's hex or dec
+
+
+
+    /* ---- Enum for legibility purposes ---- */
+    enum Token {
+        LABEL = 0,
+        OPCODE = 1,
+        OPERANDS = 2,
+        COMMENTS = 3
+    };
 
 
 };
